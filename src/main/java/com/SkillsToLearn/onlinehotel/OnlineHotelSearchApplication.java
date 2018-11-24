e8b374202222d31901bd454cc78324eba50d7581
prasanjit <prasanjit@BLR1-MAC-N03220.local>
2018-11-24 15:31:06 +0530

    First Project

---
+++ src/main/java/com/SkillsToLearn/onlinehotel/OnlineHotelSearchApplication.java
@@ -0,0 +1,12 @@
+package com.SkillsToLearn.onlinehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineHotelSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineHotelSearchApplication.class, args);
+	}
+}
---
+++ src/main/java/com/SkillsToLearn/onlinehotel/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.SkillsToLearn.onlinehotel.dao;
+
+import java.util.List;
+
+import com.SkillsToLearn.onlinehotel.formbean.UserFormBean;
+import com.SkillsToLearn.onlinehotel.model.UserBean;
+
+public interface UserDao {
+	
+	public Long getMaxUserId();
+	public UserBean findUserByUserName(String userName);
+	public UserBean findUserByEmail(String email);
+	public List<UserBean> getUsers();
+	public UserBean createUser(UserFormBean form);
+
+}
